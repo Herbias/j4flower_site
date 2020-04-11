16269192b025f8e0443b05acb273efdad90c5c0a
Adrian Manuel T. Cleofe <rastaman8693@gmail.com>
2020-04-11 17:57:27 +0800

    Created Index Page

---
+++ pages/Index.js
@@ -0,0 +1,7 @@
+import MainLayout from "../layouts/MainLayout"
+
+export default () => (
+    <MainLayout>
+      <h1 className="text-5xl font-bold text-purple-500">Hello world!</h1>
+    </MainLayout>
+  )
