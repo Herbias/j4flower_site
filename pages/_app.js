62fad156414f2f8c458ba2eda8df83fae853a550
Adrian Manuel T. Cleofe <rastaman8693@gmail.com>
2020-04-20 18:18:39 +0800

    Configure redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import App from 'next/app';
-import '../css/index.css';
+import React from "react";
+import App from "next/app";
+import "../css/index.css";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Provider } from "react-redux";
+import withRedux from "next-redux-wrapper";
+import { initStore } from "../redux/store/store";
+
+const MyApp = (props) => {
+  const { Component, pageProps, store } = props;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+};
+
+export default withRedux(initStore)(MyApp);
