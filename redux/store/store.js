4f6064dde9906afdc8f1dc671a933a59e621ce6b
Adrian Manuel T. Cleofe <rastaman8693@gmail.com>
2020-07-17 20:35:57 +0800

    update redux-wrapper

--- redux/store/store.js
+++ redux/store/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
+import { HYDRATE, createWrapper } from 'next-redux-wrapper'
 
 import rootReducer from "../reducers/index";
 
@@ -10,6 +11,22 @@ const bindMiddleware = (middleware) => {
   return applyMiddleware(...middleware);
 };
 
-export const initStore = () => {
+const reducer = (state, action) => {
+  console.log(state);
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state, // use previous state
+      ...action.payload, // apply delta from hydration
+    }
+    //if (state.count) nextState.count = state.count // preserve count value on client side navigation
+    return nextState
+  } else {
+    return rootReducer(state, action)
+  }
+}
+
+const initStore = () => {
   return createStore(rootReducer, bindMiddleware([]));
 };
+
+export const wrapper = createWrapper(initStore)
