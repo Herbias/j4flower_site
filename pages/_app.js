ed2f40332ff6243482f588d6624956a9bc1069ac
Adrian Manuel T. Cleofe <rastaman8693@gmail.com>
2020-07-17 20:40:14 +0800

    update redux-wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,15 @@ import React from "react";
 import App from "next/app";
 import "../css/index.css";
 
-import { Provider } from "react-redux";
-import withRedux from "next-redux-wrapper";
-import { initStore } from "../redux/store/store";
+import {wrapper} from "../redux/store/store";
+
 
 const MyApp = (props) => {
   const { Component, pageProps, store } = props;
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <Component {...pageProps} />
   );
 };
 
-export default withRedux(initStore)(MyApp);
+//export default withRedux(initStore)(MyApp);
+export default wrapper.withRedux(MyApp)
