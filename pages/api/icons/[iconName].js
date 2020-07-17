df8fdbcf568e9e3d94f04f1236133c70a9c51a01
Adrian Manuel T. Cleofe <rastaman8693@gmail.com>
2020-07-17 20:31:38 +0800

    Added cors for compliance of NextJS requirement

--- pages/api/icons/[iconName].js
+++ pages/api/icons/[iconName].js
@@ -1,14 +1,26 @@
 import icons from "./icons.json";
+import initMiddleware from '../../../lib/init-middleware'
+import Cors from 'cors'
 
-export default (req, res) => {
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
+
+
+export default async function handler(req, res) {
+  await cors(req, res);
   const name = req.query.iconName;
   let path = "";
 
   path = icons.filter((icon) => {
-    path = Object.keys(icon).includes(name)
-      ? res.json([icon[name].path])
-      : "invalid";
+    path = Object.keys(icon).includes(name) ?
+      res.json([icon[name].path]) :
+      "invalid";
   });
 
   // res.json([path]);
-};
+}
