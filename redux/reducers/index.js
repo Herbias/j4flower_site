7ffc0e09e7ba889ba46ed204d6df41a8aae00b25
herbias <rastaman8693@gmail.com>
2020-08-03 11:55:35 +0800

    Added QuickBuildReducer

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,7 +1,12 @@
 import NavigationReducer from "./NavigationReducer";
 import UserReducer from "./UserReducer";
+import QuickBuildReducer from "./QuickBuildReducer";
 import { combineReducers } from "redux";
 
-const rootReducer = combineReducers({ NavigationReducer, UserReducer });
+const rootReducer = combineReducers({
+  NavigationReducer,
+  UserReducer,
+  QuickBuildReducer,
+});
 
 export default rootReducer;
