bf533349486c445f03dfeaab95ac6a0e6a7df3cd
herbias <rastaman8693@gmail.com>
2020-08-11 17:14:33 +0800

    remove dependency to parent component

--- components/Products/ProductCard.jsx
+++ components/Products/ProductCard.jsx
@@ -1,10 +1,22 @@
 import { useIconHook } from "../../hooks/iconHook";
 import CustomButton from "../CustomButton";
-import { useState, useCallback, useEffect } from "react";
 
-export default function (props) {
-  const { name, image, price, index } = props;
+import Link from "next/link";
+import { useDispatch } from "react-redux";
+import { AddToCart, ToUpdate } from "../../redux/actions/CartAction";
+import { useAddToCartHook } from "../../hooks/addToCartHook";
+import { useEffect } from "react";
+
+const ProductCard = (props) => {
+  const { name, image, price, index, data } = props;
   const heartIcon = useIconHook("heart");
+
+  const dispatch = useDispatch();
+  const [loading, success] = useAddToCartHook(data);
+
+  useEffect(() => {
+    dispatch(ToUpdate(false));
+  }, [success]);
 
   return (
     <div
@@ -13,17 +25,34 @@ export default function (props) {
     >
       <img className="object-contain h-40" src={`/product/${image}`} />
       <div className="p-2 m-auto text-center">
-        <h4 className="truncate">{name && name}</h4>
+        <Link
+          href={`/shop/${data.categoryName}/${data.name.replace(/\s/g, "-")}/${
+            data.id
+          }`}
+        >
+          <a>
+            <h4 className="truncate">{name && name}</h4>
+          </a>
+        </Link>
+
         <h6>&#8369;{price && price.toFixed(2)}</h6>
       </div>
-      <div className="flex">
-        <CustomButton
-          classNames="w-40 p-2 inline-block bg-teal-300 text-white border-2 border-white cursor-pointer"
-          size="5"
-          view="20"
-          icon={heartIcon}
-        />
+      <div
+        className="flex items-center justify-center content-center"
+        onClick={(e) => dispatch(AddToCart(true, 1, data))}
+      >
+        <div className="flex content-center self-auto items-center">
+          <CustomButton
+            classNames="flex w-40 h-8 p-full bg-teal-300 text-white border-2 border-white cursor-pointer"
+            size="5"
+            view="20"
+            icon={heartIcon}
+            data={data}
+          />
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default React.memo(ProductCard);
