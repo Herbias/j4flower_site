e5032ea88c163ef64896ae8c1c2029075fb5b7fa
herbias <rastaman8693@gmail.com>
2020-08-11 17:05:09 +0800

    Added cart

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,12 +1,14 @@
 import NavigationReducer from "./NavigationReducer";
 import UserReducer from "./UserReducer";
 import QuickBuildReducer from "./QuickBuildReducer";
+import CartReducer from "./CartReducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   NavigationReducer,
   UserReducer,
   QuickBuildReducer,
+  CartReducer,
 });
 
 export default rootReducer;
